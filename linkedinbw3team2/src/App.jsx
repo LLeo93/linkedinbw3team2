b8156cf54e621ff691c46eb98a05289be35f14b9
LLeo93 <liba.leoncini@gmail.com>
2025-04-24 12:02:18 +0200

    fontawesome

--- linkedinbw3team2/src/App.jsx
+++ linkedinbw3team2/src/App.jsx
@@ -13,6 +13,7 @@ import './App.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   const dispatch = useDispatch();
